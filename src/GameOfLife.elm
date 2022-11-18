82d0ace35f226b5a1be3b40943fb61ea701e3a64
Sean Russell <slrussel@rams.colostate.edu>
2022-11-17 18:15:53 -0700

    Ran formatting

--- src/GameOfLife.elm
+++ src/GameOfLife.elm
@@ -1,21 +1,36 @@
-module GameOfLife exposing (Cell, Board, nextBoard)
+module GameOfLife exposing (Board, Cell, nextBoard)
 
 import List exposing (range)
 import Set exposing (..)
 
-type alias Cell = (Int, Int)
-type alias Board = Set Cell
+
+type alias Cell =
+    ( Int, Int )
+
+
+type alias Board =
+    Set Cell
+
 
 nearby : Cell -> Board
-nearby (x,y) = map (\n -> (x-1+modBy 3 n, y-1+n//3)) (fromList (range 0 8))
+nearby ( x, y ) =
+    map (\n -> ( x - 1 + modBy 3 n, y - 1 + n // 3 )) (fromList (range 0 8))
+
 
 cellWillBeAlive : Board -> Cell -> Bool
 cellWillBeAlive board cell =
-  let neighbors = size (diff (intersect board (nearby cell)) (singleton cell))
-  in neighbors == 3 || (neighbors == 2 && member cell board)
+    let
+        neighbors =
+            size (intersect board (diff (nearby cell) (singleton cell)))
+    in
+    neighbors == 3 || (neighbors == 2 && member cell board)
+
 
 cellsToCheck : Board -> Board
-cellsToCheck = foldl (nearby >> union) empty
+cellsToCheck =
+    foldl (nearby >> union) empty
+
 
 nextBoard : Board -> Board
-nextBoard board = filter (cellWillBeAlive board) (cellsToCheck board)
+nextBoard board =
+    filter (cellWillBeAlive board) (cellsToCheck board)
--- src/Main.elm
+++ src/Main.elm
@@ -1,87 +1,108 @@
 module Main exposing (..)
 
-import GameOfLife exposing (..)
-import ExampleBoards exposing (..)
-import RenderBoard exposing (..)
-
 import Browser
-import Html exposing (Html,div,text,button)
+import ExampleBoards exposing (..)
+import GameOfLife exposing (..)
+import Html exposing (Html, button, div, text)
 import Html.Attributes exposing (style)
 import Html.Events exposing (onClick)
 import Process
 import Random
+import RenderBoard exposing (..)
 import Set exposing (fromList)
 import Task
 
-type Msg = SleepComplete
-         | RandomBoardGenerated (List Cell)
-         | LoadBoard Board
-         | GenBoard
 
-type alias Model = Board
+type Msg
+    = SleepComplete
+    | RandomBoardGenerated (List Cell)
+    | LoadBoard Board
+    | GenBoard
+
+
+type alias Model =
+    Board
+
 
 randomBoardGenerator : Random.Generator (List Cell)
-randomBoardGenerator = Random.list 500 (Random.pair (Random.int 40 60) (Random.int 40 60))
+randomBoardGenerator =
+    Random.list 500 (Random.pair (Random.int 40 60) (Random.int 40 60))
+
 
 sleep : Cmd Msg
-sleep = Process.sleep 100 |> Task.perform (always SleepComplete)
+sleep =
+    Process.sleep 100 |> Task.perform (always SleepComplete)
 
-initialModel = (fromList [], Random.generate RandomBoardGenerated randomBoardGenerator)
 
-update : Msg -> Model -> (Model, Cmd Msg)
+initialModel : ( Model, Cmd Msg )
+initialModel =
+    ( fromList [], Random.generate RandomBoardGenerated randomBoardGenerator )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    SleepComplete -> (nextBoard model, sleep)
-    RandomBoardGenerated cells -> (fromList cells, sleep)
-    LoadBoard board -> (board, sleep)
-    GenBoard -> (model, Random.generate RandomBoardGenerated randomBoardGenerator)
+    case msg of
+        SleepComplete ->
+            ( nextBoard model, sleep )
+
+        RandomBoardGenerated cells ->
+            ( fromList cells, sleep )
+
+        LoadBoard board ->
+            ( board, sleep )
+
+        GenBoard ->
+            ( model, Random.generate RandomBoardGenerated randomBoardGenerator )
+
 
 selectorButton : msg -> String -> Html msg
 selectorButton msg description =
-  button 
-    [ onClick msg 
-    , style "margin" ".4em .7em"
-    , style "padding" ".4em 1em"
-    , style "font-size" "large"
-    , style "font-weight" "200"
-    , style "border-radius" "0.2em"
-    , style "border-width" "thin"
-    , style "border-style" "solid"
-    , style "border-color" "silver"
-    , style "background-color" "#ffffffbb"
-    , style "color" "#112233"
-    ]
-    [ text description]
+    button
+        [ onClick msg
+        , style "margin" ".4em .7em"
+        , style "padding" ".4em 1em"
+        , style "font-size" "large"
+        , style "font-weight" "200"
+        , style "border-radius" "0.2em"
+        , style "border-width" "thin"
+        , style "border-style" "solid"
+        , style "border-color" "silver"
+        , style "background-color" "#ffffffbb"
+        , style "color" "#112233"
+        ]
+        [ text description ]
+
 
 view : Board -> Html Msg
 view b =
-  div
-    [ style "aspect-ratio" "1/1"
-    , style "width" "100%"
-    , style "position" "relative"
-    ]
-    [ renderBoard b
-    , div 
-        [ style "position" "absolute"
-        , style "bottom" "0"
+    div
+        [ style "aspect-ratio" "1/1"
         , style "width" "100%"
-        , style "display" "flex"
-        , style "justify-content" "center"
-        , style "flex-wrap" "wrap"
+        , style "position" "relative"
         ]
-        [ selectorButton GenBoard "Randomize"
-        , selectorButton (LoadBoard glider) "Glider"
-        , selectorButton (LoadBoard explosion) "Explosion" 
-        , selectorButton (LoadBoard methuselah) "Methuselah" 
-        , selectorButton (LoadBoard gliderGun) "Glider Gun" 
+        [ renderBoard b
+        , div
+            [ style "position" "absolute"
+            , style "bottom" "0"
+            , style "width" "100%"
+            , style "display" "flex"
+            , style "justify-content" "center"
+            , style "flex-wrap" "wrap"
+            ]
+            [ selectorButton GenBoard "Randomize"
+            , selectorButton (LoadBoard glider) "Glider"
+            , selectorButton (LoadBoard explosion) "Explosion"
+            , selectorButton (LoadBoard methuselah) "Methuselah"
+            , selectorButton (LoadBoard gliderGun) "Glider Gun"
+            ]
         ]
-    ]
+
 
 main : Program () Model Msg
 main =
-  Browser.element
-    { init = always initialModel
-    , view = view
-    , update = update
-    , subscriptions = always Sub.none
-    }
+    Browser.element
+        { init = always initialModel
+        , view = view
+        , update = update
+        , subscriptions = always Sub.none
+        }
